e420a067f36a79f84f77e3408455d031c83e0361
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-03-15 12:13:26 +0300

    Setup basic home redux slice

---
+++ src/Redux/home/homeSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+export const homeSlice = createSlice({
+  name: 'home',
+  initialState,
+  reducers: {
+  },
+  extraReducers(builder) {
+    builder
+  },
+});
+
+export default homeSlice.reducer;
+
